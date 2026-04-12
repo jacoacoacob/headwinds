54586fcf665ae00dc94898d6962ecd47ec5469ad
jacoacoacob <jacoacoacob@protonmail.com>
2026-04-12 13:04:42 -0500

    removed stations data files whose 'features' arrays were empty

--- scripts/clean-stations.js
+++ scripts/clean-stations.js
@@ -1,12 +1,27 @@
-
 import path from "node:path"
 import fs from "node:fs"
 
 const DATA_DIR = path.resolve(import.meta.dirname, process.env.DATA_DIR || "../data");
 
 function run() {
+  const pathsToDelete = [];
+
   for (let filename of fs.readdirSync(DATA_DIR)) {
-    console.log(filename)
+    const filepath = path.join(DATA_DIR, filename);
+    
+    const file = fs.readFileSync(filepath, {
+      encoding: "utf-8"
+    });
+
+    const data = JSON.parse(file);
+
+    if (data.features.length === 0) {
+      pathsToDelete.push(filepath);
+    }
+  }
+
+  for (let filepath of pathsToDelete) {
+    fs.rmSync(filepath);
   }
 }
 
